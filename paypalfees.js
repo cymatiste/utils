9e3f5064cdf13afac3830e3b511ad9aa2d80e14b
Sarah Imrisek <sarah@sinkingship.ca>
2018-01-03 13:46:19 -0500

    ~ prettier commenting

--- paypalfees.js
+++ paypalfees.js
@@ -4,7 +4,8 @@
 
     A calculator for sending payments for goods and services from Canada to the US
 
-    Outputs to console an amount adjusted so that, after paypal charges the recipient all applicable fees, the remaining balance will be the intended payment.
+    Outputs to console an amount adjusted so that, after paypal charges the recipient all applicable fees, 
+    the remaining balance will be the intended payment.
 
     Accurate to within $0.01 as of January 3 2018 (based on a sample size of 2 payments)
 
